2e203caff23bbb6551fa4c2db8d4d46609fabbf4
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-11-10 23:31:44 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,20 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState ("");
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container text-center mt-5">
+			<h1>todos</h1>
+			<div>
+			<input type="text" onChange={(e)=> setTarea(e.target.value)} value={tarea} placeholder="What needs to be done?"/> 
+			</div>	
+			<h3>La tarea es {tarea}</h3>
+			<button className="btn-primary mt-3" onClick={ () =>  {
+				setTarea("")
+			}}>Borrar Tarea</button>
 		</div>
 	);
 };
