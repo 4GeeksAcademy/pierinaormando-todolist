52cffcf6b19c8222988172733d7ff6b56bad95f6
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-11-13 23:32:19 +0000

    corrección onMouseEnter y onMouse Leave, terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	const [tarea, setTarea] = useState ("");
+	const [inputValue, setInputValue] = useState("");
+	const [toDos, setToDos] = useState([]);
+	const [tarea, setsetTarea] = useState(null);
+
+	const changeInputValue = (e) => {
+		setInputValue(e.target.value);
+	};
+
+	const addInputValue = (e) => {
+		if (e.key === "Enter") {
+			//para agregar mas elementos al array sin modificar el estado del anterior
+			setToDos([...toDos, inputValue]);
+			setInputValue("");
+		}
+	};
+
+	const deleteTask = (index) => {
+		setToDos(toDos.filter((t, currentIndex) => index !== currentIndex));
+	};
 
 	return (
-		<div className="container text-center mt-5">
-			<h1>todos</h1>
-			<div>
-			<input type="text" onChange={(e)=> setTarea(e.target.value)} value={tarea} placeholder="What needs to be done?"/> 
-			</div>	
-			<h3>La tarea es {tarea}</h3>
-			<button className="btn-primary mt-3" onClick={ () =>  {
-				setTarea("")
-			}}>Borrar Tarea</button>
+		<div className="text-center container">
+			<h1>ToDoList</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						placeholder="What do you need to do?"
+						value={inputValue}
+						onChange={changeInputValue}
+						onKeyDown={addInputValue}
+					/>
+				</li>
+				{toDos.map((t, index) => (
+					<li
+						key={index}
+						onMouseEnter={() => setsetTarea(index)}
+						onMouseLeave={() => setsetTarea(null)}
+					>
+						{t}
+						{tarea === index && (
+							<i
+								className="far fa-trash-alt m-1"
+								onClick={() => deleteTask(index)}
+							></i>
+						)}
+					</li>
+				))}
+			</ul>
+			<div className="tasks">{toDos.length} item left</div>
 		</div>
 	);
 };
